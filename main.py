0fa053bbd418bc19954770d937a9e076ca2ddfe3
justin830827 <justin801514@gmail.com>
2019-11-17 17:51:16 -0500

    policy complete

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 from copy import copy, deepcopy
+from operator import itemgetter
 
 random.seed(123)
 
@@ -112,11 +113,72 @@ def plot_simulation(series, total_node, title):
     plt.close()
 
 
-def compute_eigenvalue(adj):
+def compute_eigen(adj):
     eigenvalue, eigenvector = np.linalg.eig(adj)
     eig_set = [(eigenvalue[i], eigenvector[i]) for i in range(len(eigenvalue))]
     eig_set = sorted(eig_set, key=lambda x: x[0], reverse=1)
     return eig_set
+
+
+def policyA(adj, g, k=200):
+    immun = set()
+    total_node = nx.number_of_nodes(g)
+    while len(immun) < k:
+        r = random.randint(0, total_node-1)
+        if r not in immun:
+            immun.add(r)
+
+    for node in immun:
+        for i in range(len(adj[node])):
+            adj[node][i] = 0
+            adj[i][node] = 0
+    return adj
+
+
+def policyB(adj, g, k=200):
+    immun = set()
+    degree = sorted(list(g.degree()), key=lambda x: x[1], reverse=1)
+    for i in range(k):
+        immun.add(degree[i][0])
+
+    for node in immun:
+        for i in range(len(adj[node])):
+            adj[node][i] = 0
+            adj[i][node] = 0
+    return adj
+
+
+def policyC(adj, g, k=200):
+    immun = set()
+    total_node = nx.number_of_nodes(g)
+    while len(immun) < k:
+        degree = sorted(list(g.degree()), key=lambda x: x[1], reverse=1)
+        immun.add(degree[0][0])
+        g.remove_node(degree[0][0])
+
+    adjC = [[0 for _ in range(total_node)] for _ in range(total_node)]
+    for i in nx.edges(g):
+        adjC[i[0]][i[1]] = 1
+        adjC[i[1]][i[0]] = 1
+    return adjC
+
+
+def policyD(adj, g, k=200):
+    total_node = nx.number_of_nodes(g)
+    val, vec = np.linalg.eig(adj)
+    eig_set = [(val[i], vec[i]) for i in range(len(val))]
+    eig_set = sorted(eig_set, key=lambda x: x[0], reverse=1)
+    largest_vec = eig_set[0][1]
+    largest_vec = np.absolute(largest_vec)
+    target = [u[0]
+              for u in sorted(enumerate(largest_vec), reverse=True, key=itemgetter(1))[:k]]
+    for i in target:
+        g.remove_node(i)
+    adjD = [[0 for _ in range(total_node)] for _ in range(total_node)]
+    for i in nx.edges(g):
+        adjD[i[0]][i[1]] = 1
+        adjD[i[1]][i[0]] = 1
+    return adjD
 
 
 def main():
@@ -138,9 +200,9 @@ def main():
     for i in nx.edges(g):
         adj[i[0]][i[1]] = 1
         adj[i[1]][i[0]] = 1
-    eigen_set = compute_eigenvalue(adj)
 
     # # a. Evaluate infection spread across the network
+    # eigen_set = compute_eigen(adj)
     # largest_eigenvalue = eigen_set[0][0].real
     # s1 = largest_eigenvalue * Cvpm1
     # s2 = largest_eigenvalue * Cvpm2
@@ -187,7 +249,7 @@ def main():
     # # beta1 and delta1 results
     # first_series = []
     # for i in range(10):
-    #     res = simulation(g, beta1, delta1, adj_matrix)
+    #     res = simulation(g, beta1, delta1, adj)
     #     first_series.append(res)
     # plot_simulation(first_series, total_node, 'Simulation with β = {} and δ = {}'.format(
     #     beta1, delta1))
@@ -197,12 +259,56 @@ def main():
     # # beta2 and delta2 results
     # second_series = []
     # for i in range(10):
-    #     res = simulation(g, beta2, delta2, adj_matrix)
+    #     res = simulation(g, beta2, delta2, adj)
     #     second_series.append(res)
     # plot_simulation(second_series, total_node, 'Simulation with β = {} and δ = {}'.format(
     #     beta2, delta2))
 
     # Part 3:  Implements immunization policies
+    # d. calculate the effective strength (s) of the virus on the immunized contact network
+    print("\n\nImmunization wiht Policy A.....................")
+    adj_matrixA = policyA(adj.copy(), g.copy())
+    eigen_set_A = compute_eigen(adj_matrixA)
+    largest_eigenvalue_A = eigen_set_A[0][0].real
+    sA_1 = largest_eigenvalue_A * Cvpm1
+    sA_2 = largest_eigenvalue_A * Cvpm2
+    print("Effective strengh of Policy A with beta = {}, delta = {} is: {}".format(
+        beta1, delta1, sA_1))
+    print("Effective strengh of Policy A with beta = {}, delta = {} is: {}\n".format(
+        beta2, delta2, sA_2))
+
+    print("\n\nImmunization wiht Policy B.....................")
+    adj_matrixB = policyB(adj.copy(), g.copy())
+    eigen_set_B = compute_eigen(adj_matrixB)
+    largest_eigenvalue_B = eigen_set_B[0][0].real
+    sB_1 = largest_eigenvalue_B * Cvpm1
+    sB_2 = largest_eigenvalue_B * Cvpm2
+    print("Effective strengh of Policy B with beta = {}, delta = {} is: {}".format(
+        beta1, delta1, sB_1))
+    print("Effective strengh of Policy B with beta = {}, delta = {} is: {}\n".format(
+        beta2, delta2, sB_2))
+
+    print("\n\nImmunization wiht Policy C.....................")
+    adj_matrixC = policyC(adj.copy(), g.copy())
+    eigen_set_C = compute_eigen(adj_matrixC)
+    largest_eigenvalue_C = eigen_set_C[0][0].real
+    sC_1 = largest_eigenvalue_C * Cvpm1
+    sC_2 = largest_eigenvalue_C * Cvpm2
+    print("Effective strengh of Policy C with beta = {}, delta = {} is: {}".format(
+        beta1, delta1, sC_1))
+    print("Effective strengh of Policy C with beta = {}, delta = {} is: {}\n".format(
+        beta2, delta2, sC_2))
+
+    print("\n\nImmunization wiht Policy D.....................")
+    adj_matrixD = policyD(adj.copy(), g.copy())
+    eigen_set_D = compute_eigen(adj_matrixD)
+    largest_eigenvalue_D = eigen_set_D[0][0].real
+    sD_1 = largest_eigenvalue_D * Cvpm1
+    sD_2 = largest_eigenvalue_D * Cvpm2
+    print("Effective strengh of Policy D with beta = {}, delta = {} is: {}".format(
+        beta1, delta1, sD_1))
+    print("Effective strengh of Policy D with beta = {}, delta = {} is: {}\n".format(
+        beta2, delta2, sD_2))
 
 
 if __name__ == "__main__":
